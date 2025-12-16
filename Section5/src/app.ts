b8b07ccf2f3b956d71056c379131f3c717c5f259
thuanvo2405 <vothuan2405003@gmail.com>
2025-12-16 17:47:34 +0700

    Section 5

---
+++ Section5/src/app.ts
@@ -0,0 +1,12 @@
+// Code goes here!
+const add = (a: number = 2, b: number) => console.log(a + b);
+
+// add(2); in correct because we should defined default value after other paramaters
+
+const person = {
+  name: "Thuan",
+  age: 23,
+};
+
+// const copiedPerson = person; copy same address memory
+// const copiedPerson = { ...person }; copy value but not same address memory
